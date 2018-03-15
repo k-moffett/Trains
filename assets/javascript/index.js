a04aa5520e0de5884949aebd77c06fb9dd493198
Kurtis Moffett <moffkr91@gmail.com>
2018-03-15 15:21:34 -0700

    quicksave

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,30 +1,50 @@
 $( document ).ready(function () {
 
 let config = {
-    apiKey: "AIzaSyAqCqntoJGH1crGl31JN8pFiWB_QbmHpGc",
-    authDomain: "time-sheet-55702.firebaseapp.com",
-    databaseURL: "https://time-sheet-55702.firebaseio.com",
-    projectId: "time-sheet-55702",
+    apiKey: "AIzaSyAaZ2r7nPtR3amCUPeYi7DuF7Z0yBVYV6E",
+    authDomain: "trains-9f9f1.firebaseapp.com",
+    databaseURL: "https://trains-9f9f1.firebaseio.com",
+    projectId: "trains-9f9f1",
     storageBucket: "",
-    messagingSenderId: "723033960582"
+    messagingSenderId: "735348772785"
   };
   firebase.initializeApp(config);
   let database =  firebase.database()
+
 $("#submit").on("click", function() {
     event.preventDefault();
+    let name = $("#train-name").val().trim()
+    let destination = $("#destination").val().trim()
+    let train_time = $("#train-time").val().trim()
+    let frequency = $("#frequency").val().trim()
+    let first_time = $("#train-time")
+   
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: train_time,
+        frequency: frequency,
+        first_time: first_time,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+    })
+    $("#train-name").val("")
+    $("#destination").val("")
+    $("#train-time").val("")
+    $("#frequency").val("")
 })
-})
+
+
+
+
+
+
 get_dataz()
 function get_dataz(){
-    database.ref().on("child_added", function(snapshot) {
-        let all_users = snapshot.val()
-        let users_array = Array.of(all_users)
-       for (i=0; i<users_array.length; i++){
-          $("#t-body").append(`<tr><td>`+users_array[i].employee_namez+`</td><td>`+users_array[i].employee_role+`</td><td>`+users_array[i].employee_start_date+`</td><td></td><td>`+users_array[i].employee_monthly_rate+`</td></tr>`)
-       }
-    }),
-    function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      }
-}
+    database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+        console.log(snapshot.val())
+        let response = snapshot.val()       
+        $("#display-schedule").append(`<tr> <td class="col">`+response.name+`</td> <td class="col">`+response.destination+`</td> <td class="col">`+response.frequency+`</td> <td class="col"></td> <td class="col"></td> </tr>`)
+        })
+    }
+//
 })
