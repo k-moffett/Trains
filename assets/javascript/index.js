3e5786c81bb5a4d62eb22216160ee048d328cd83
Kurtis Moffett <moffkr91@gmail.com>
2018-03-16 15:42:52 -0700

    quicksave

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -40,13 +40,22 @@ $("#submit").on("click", function() {
 
 
 
-function convert_to_minutes(h1,m1,h2,m2 ){
+function greater_get_minutes(h1,m1,h2,m2 ){
     total_first_min = (h1*60)+(m1*1)
     total_current_time_min = (h2*60)+(m2*1)
     minute_difference = (total_first_min*1) - (total_current_time_min*1)
-    console.log(minute_difference, "minute difference")
+    return (minute_difference*1)
 }
-
+function lesser_get_minutes(h1,m1,h2,m2 ){
+    total_first_min = (h1*60)+(m1*1)
+    console.log(total_first_min,"first time")
+    total_current_time_min = (h2*60)+(m2*1)
+    console.log(total_current_time_min,"total time")
+    minute_difference = (total_current_time_min*1) - (total_first_min*1)
+    console.log(minute_difference, "minute difference")
+    total_minutes_left = 1440 - (minute_difference*1)
+    return (total_minutes_left*1)
+}
 
 
 
@@ -65,17 +74,25 @@ function get_dataz(){
     database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
         let response = snapshot.val()
         let next_arrival;
+        let minutes_left;
         starting_time = response.first_time.toString().replace(":","")
         string_time = current_time.toString().replace(":","")
 
+        
         math_time = current_time.toString().split(":")
         math_response = response.first_time.toString().split(":")
 
+
         if (starting_time > string_time){
-        next_arrival = convert_to_minutes(math_response[0],math_response[1],math_time[0],math_time[1])
+        minutes_left = greater_get_minutes(math_response[0],math_response[1],math_time[0],math_time[1])
+        } else if (starting_time < string_time){
+        minutes_left = lesser_get_minutes(math_response[0],math_response[1],math_time[0],math_time[1])
         }
 
-        $("#display-schedule").append(`<tr> <td class="col">`+response.name+`</td> <td class="col">`+response.destination+`</td> <td class="col">`+response.frequency+`</td> <td class="col">`+next_arrival+`</td> <td class="col"></td> </tr>`)
+
+
+
+        $("#display-schedule").append(`<tr> <td class="col">`+response.name+`</td> <td class="col">`+response.destination+`</td> <td class="col">`+response.frequency+`</td> <td class="col">`+next_arrival+`</td> <td class="col">`+minutes_left+`</td> </tr>`)
         })
     }
 
