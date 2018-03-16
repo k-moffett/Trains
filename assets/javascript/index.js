58d339a1d8f4f6ee7cf5fb0a469f70dc2a0a1012
Kurtis Moffett <moffkr91@gmail.com>
2018-03-16 15:17:20 -0700

    minute difference

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -40,6 +40,14 @@ $("#submit").on("click", function() {
 
 
 
+function convert_to_minutes(h1,m1,h2,m2 ){
+    total_first_min = (h1*60)+(m1*1)
+    total_current_time_min = (h2*60)+(m2*1)
+    minute_difference = (total_first_min*1) - (total_current_time_min*1)
+    console.log(minute_difference, "minute difference")
+}
+
+
 
 
 
@@ -55,9 +63,19 @@ function time(){
 }
 function get_dataz(){
     database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
-        console.log(snapshot.val())
-        let response = snapshot.val()  
-        $("#display-schedule").append(`<tr> <td class="col">`+response.name+`</td> <td class="col">`+response.destination+`</td> <td class="col">`+response.frequency+`</td> <td class="col"></td> <td class="col"></td> </tr>`)
+        let response = snapshot.val()
+        let next_arrival;
+        starting_time = response.first_time.toString().replace(":","")
+        string_time = current_time.toString().replace(":","")
+
+        math_time = current_time.toString().split(":")
+        math_response = response.first_time.toString().split(":")
+
+        if (starting_time > string_time){
+        next_arrival = convert_to_minutes(math_response[0],math_response[1],math_time[0],math_time[1])
+        }
+
+        $("#display-schedule").append(`<tr> <td class="col">`+response.name+`</td> <td class="col">`+response.destination+`</td> <td class="col">`+response.frequency+`</td> <td class="col">`+next_arrival+`</td> <td class="col"></td> </tr>`)
         })
     }
 
